905485832b70b6b05c20f5d795cd1d8f2d995630
HastiHomayooni <homayooni.hasti@gmail.com>
2023-10-25 13:28:10 +0330

    use regex for validation in login and signup

--- src/Login.js
+++ src/Login.js
@@ -1,28 +1,72 @@
 import React from 'react';
 import './App.css';
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 function Login(){
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const [emailV,setEmailV] = useState(true);
+    const [passwordV,setPasswordV] = useState(true);
+
+    const emailRegex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+    const passwordRegex = /^\S{6,}$/;
+
+    function changeEmail(e){
+        const value = e.target.value;
+        const isValid = emailRegex.test(value);
+        if(value==="") {
+            setEmailV(true)
+        }else{
+            setEmailV(isValid);
+        }
+        setEmail(value);
+    }
+    function changePassword(e){
+        const value = e.target.value;
+        const isValid = passwordRegex.test(value);
+        if(value==="") {
+            setPasswordV(true)
+        }else{
+            setPasswordV(isValid);
+        }
+        setPassword(value);
+    }
     return(
         <div className='w-screen h-screen bg-gray-100 flex items-center justify-center sm:p-4'>
             <div className='w-screen h-screen sm:w-auto sm:h-auto bg-white flex flex-col items-center justify-center sm:rounded-3xl p-4 sm:px-20 sm:py-10'>
                 <div className='w-auto font-imprima text-myDark text-3xl text-center'><b>Log In Now</b></div>
                 <div className='w-auto text-center font-imprima text-sm text-gray-500 mt-3'>please login to continue our app</div>
                 <div className='flex flex-col my-5 w-auto'>
-                    <input className='border-2 placeholder:font-imprima placeholder:text-center text-center border-myDark w-auto mx-2 my-2 mt-4 md:px-6 md:py-4 px-3 py-3 rounded-3xl hover:border-myOrange focus:border-myOrange focus:outline-none' type='email' placeholder='Email'/>
-                    <input className='border-2 placeholder:font-imprima placeholder:text-center text-center border-myDark w-auto mx-2 mt-2 md:px-6 md:py-4 px-3 py-3 rounded-3xl hover:border-myOrange focus:border-myOrange focus:outline-none' type='password' placeholder='Password'/>
+                    <div className='flex flex-col w-auto'>
+                        <input className='border-2 placeholder:font-imprima placeholder:text-center text-center bg-gray-100 border-gray-100 w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl focus:border-myDark focus:outline-none'
+                            value={email} onChange={e => changeEmail(e)} type='email' placeholder='Email'/>
+                        {email ==='' && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>You should fill the email.</div>)}
+                        {email !=='' && !emailV && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>Invalid email.</div>)}
+                    </div>
+                    
+                    <div className='flex flex-col w-auto'>
+                        <input className='border-2 placeholder:font-imprima placeholder:text-center text-center bg-gray-100 border-gray-100 w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl focus:border-myDark focus:outline-none'
+                            value={password} onChange={e => changePassword(e)} onBlur={()=>{if(!passwordV) alert('Valid Password:minimum of 6 characters,except whitespace')}} type='password' placeholder='Password'/>
+                        {password ==='' && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>You should fill the password.</div>)}
+                        {password !=='' && !passwordV && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>Invalid Password</div>)}
+                    </div>
+
                     <Link className='w-auto' to={''}>
-                        <div className='text-right w-auto mx-2 md:px-6 md:py-3 px-3 py-2 text-myOrange text-sm'>Forget Password?</div>
+                        <div className='bg-myOrange rounded-3xl mx-2 py-4 px-8 w-auto text-white text-center text-lg font-imprima mt-8'>Log in</div>
                     </Link>
-                    <Link className='w-auto' to={''}>
-                        <div className='bg-myOrange rounded-3xl mx-2 py-4 px-8 w-auto text-white text-center text-lg font-imprima mt-6'>Log in</div>
-                    </Link>
-                    <div className='flex justify-between items-center mt-4 mx-4'>
+
+                    <div className='flex justify-between items-center mt-8 mx-4'>
                         <div className='text-myDark w-auto text-sm mr-2 ml-auto'>Don't have an account ?</div>
                         <Link className='w-auto' to={'/SignUp'}>
                             <div className='text-myOrange ml-2 text-sm mr-auto'><b>Sign Up</b></div>
                         </Link>
                     </div>
+
+                    <Link className='w-auto' to={''}>
+                        <div className='text-left w-auto mx-4 text-myOrange text-sm mt-1'>Forget Password?</div>
+                    </Link>
                 </div>
             </div>
         </div>
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,8 +1,64 @@
 import React from 'react';
 import './App.css';
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 function SignUp(){
+    const [name,setName] = useState('');
+    const [username,setUsername] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const [emailV,setEmailV] = useState(true);
+    const [nameV,setNameV] = useState(true);
+    const [usernameV,setUsernameV] = useState(true);
+    const [passwordV,setPasswordV] = useState(true);
+
+    const nameRegex = /^[a-zA-Z \-']+$/;
+    const usernameRegex = /^[a-zA-Z0-9_]{3,}$/;
+    const emailRegex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+    const passwordRegex = /^\S{6,}$/;
+
+    function changeEmail(e){
+        const value = e.target.value;
+        const isValid = emailRegex.test(value);
+        if(value==="") {
+            setEmailV(true)
+        }else{
+            setEmailV(isValid);
+        }
+        setEmail(value);
+    }
+    function changeName(e){
+        const value = e.target.value;
+        const isValid = nameRegex.test(value);
+        if(value==="") {
+            setNameV(true)
+        }else{
+            setNameV(isValid);
+        }
+        setName(value);
+    }
+    function changeUsername(e){
+        const value = e.target.value;
+        const isValid = usernameRegex.test(value);
+        if(value==="") {
+            setUsernameV(true)
+        }else{
+            setUsernameV(isValid);
+        }
+        setUsername(value);
+    }
+    function changePassword(e){
+        const value = e.target.value;
+        const isValid = passwordRegex.test(value);
+        if(value==="") {
+            setPasswordV(true)
+        }else{
+            setPasswordV(isValid);
+        }
+        setPassword(value);
+    }
     return(
         <div className='w-screen h-screen bg-gray-100 flex items-center justify-center sm:p-4'>
             <div className='w-screen h-screen sm:w-auto sm:h-auto bg-white flex flex-col items-center justify-center sm:rounded-3xl p-4 sm:px-20 sm:py-10'>
@@ -10,12 +66,35 @@ function SignUp(){
                 <div className='w-auto text-center font-imprima text-sm text-gray-500 mt-3'>please fill the details and create account</div>
                 <div className='flex flex-col my-5 w-auto'>
                     <div className='flex flex-col w-auto my-2 md:flex-row'>
-                        <input className='border-2 placeholder:font-imprima placeholder:text-center text-center border-myDark w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl hover:border-myOrange focus:border-myOrange focus:outline-none' type='text' placeholder='First Name'/>
-                        <input className='border-2 placeholder:font-imprima placeholder:text-center text-center border-myDark w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl hover:border-myOrange focus:border-myOrange focus:outline-none' type='text' placeholder='Last Name'/>
+                        <div className='flex flex-col w-auto'>
+                            <input className='border-2 placeholder:font-imprima placeholder:text-center text-center bg-gray-100 border-gray-100 w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl focus:border-myDark focus:outline-none'
+                                value={name} onChange={e => changeName(e)} onBlur={()=>{if(!nameV) alert('Valid Name:letters,spaces,and hyphens')}} type='text' placeholder='Name'/>
+                            {name ==='' && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>You should fill the name.</div>)}
+                            {name !=='' && !nameV && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>Invalid Name</div>)}
+                        </div>
+                        
+                        <div className='flex flex-col w-auto'>
+                            <input className='border-2 placeholder:font-imprima placeholder:text-center text-center bg-gray-100 border-gray-100 w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl focus:border-myDark focus:outline-none'
+                                value={username} onChange={e => changeUsername(e)} onBlur={()=>{if(!usernameV) alert('Valid Username:letters,underscores,with a minimum length of 3 characters')}} type='text' placeholder='Username'/>
+                            {username ==='' && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>You should fill the username.</div>)}
+                            {username !=='' && !usernameV && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>Invalid Username</div>)}
+                        </div>                        
                     </div>
+
                     <div className='flex flex-col w-auto my-2 md:flex-row'>
-                        <input className='border-2 placeholder:font-imprima placeholder:text-center text-center border-myDark w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl hover:border-myOrange focus:border-myOrange focus:outline-none' type='email' placeholder='Email'/>
-                        <input className='border-2 placeholder:font-imprima placeholder:text-center text-center border-myDark w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl hover:border-myOrange focus:border-myOrange focus:outline-none' type='password' placeholder='Password'/>
+                        <div className='flex flex-col w-auto'>
+                            <input className='border-2 placeholder:font-imprima placeholder:text-center text-center bg-gray-100 border-gray-100 w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl focus:border-myDark focus:outline-none'
+                                value={email} onChange={e => changeEmail(e)} type='email' placeholder='Email'/>
+                            {email ==='' && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>You should fill the email.</div>)}
+                            {email !=='' && !emailV && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>Invalid email.</div>)}
+                        </div>
+                        
+                        <div className='flex flex-col w-auto'>
+                            <input className='border-2 placeholder:font-imprima placeholder:text-center text-center bg-gray-100 border-gray-100 w-auto mx-2 my-2 md:px-6 md:py-3 px-3 py-2 rounded-3xl focus:border-myDark focus:outline-none'
+                                value={password} onChange={e => changePassword(e)} onBlur={()=>{if(!passwordV) alert('Valid Password:minimum of 6 characters,except whitespace')}} type='password' placeholder='Password'/>
+                            {password ==='' && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>You should fill the password.</div>)}
+                            {password !=='' && !passwordV && (<div className='w-auto font-imprima text-left text-red-500 mx-2 md:px-6 px-3 text-xs'>Invalid Password</div>)}
+                        </div>
                     </div>
                     <Link className='w-auto' to={''}>
                         <div className='bg-myOrange rounded-3xl mx-2 py-4 px-8 w-auto text-white text-center font-imprima mt-10'>Create Account</div>
