a10b884dcaa3ef0a489b4749716cea0984268e97
HastiHomayooni <homayooni.hasti@gmail.com>
2023-11-08 16:22:24 +0330

    input date and select

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Link } from "react-router-dom";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import { useState } from 'react';
 import camera from './images/camera.png';
 import next from './images/next.png';
 
 function Profile(){
     const [Name,setName]=useState('Name')
+    const [Username,setUsername]=useState('Username')
+    const [Date, setDate] = useState(null);
     const [Camera,setCamera]=useState(camera)
     function handleChange(e) {
         console.log(e.target.files);
@@ -28,24 +32,38 @@ function Profile(){
                 <div className='w-auto flex-col items-center justify-center mx-7'>
                     <div className='w-auto flex justify-between my-5 px-2 py-1'>
                         <div className='w-auto font-imprima text-gray-400'>Name</div>
-                        <input className='w-44 sm:w-64 outline-none border-b border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}/>
+                        <input className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}
+                            value={Name}/>
                     </div>
                     <div className='w-auto flex justify-between my-5 px-2 py-1'>
                         <div className='w-auto font-imprima text-gray-400'>Username</div>
-                        <input className='w-44 sm:w-64 outline-none border-b border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}/>
+                        <input className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}
+                            value={Username}/>
                     </div>
                     <div className='w-auto flex justify-between my-5 px-2 py-1'>
-                        <div className='w-auto font-imprima text-gray-400 mr-2'>Date of birth</div>
-                        <input className='w-44 sm:w-64 outline-none border-b border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}/>
+                        <div className='w-auto font-imprima text-gray-400 mr-3'>Date of birth</div>
+                        <DatePicker
+                            className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima'
+                            style={{ borderBottomWidth: '1.5px' }}
+                            selected={Date}
+                            onChange={date => setDate(date)}
+                            placeholderText='Select a Date'
+                        />
                     </div>
                     <div className='w-auto flex justify-between my-5 px-2 py-1'>
                         <div className='w-auto font-imprima text-gray-400'>Gender</div>
-                        <input className='w-44 sm:w-64 outline-none border-b border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}/>
+                        <select className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}>
+                            <option value='female'>Female</option>
+                            <option value='male'>Male</option>
+                            <option value='others'>Others</option>
+                        </select>
                     </div>
                 </div>
                 <div className='w-auto rounded-2xl bg-myDark flex justify-between p-3 px-4 mx-7 mt-10 mb-8'>
                     <div className='w-auto font-imprima text-white text-lg text-center my-auto'>Specification</div>
-                    <img className='w-8' src={next} alt='next'/>
+                    <Link className='w-auto' to={'/Specification'}>
+                        <img className='w-8' src={next} alt='next'/>
+                    </Link>
                 </div>
                 <div className='w-auto flex justify-between mx-11'>
                     <div className='w-auto font-imprima text-gray-400 text-lg'>Skip</div>
