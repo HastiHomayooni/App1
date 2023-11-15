585ac6722e063f08050eff024463636c5d25b335
HastiHomayooni <homayooni.hasti@gmail.com>
2023-11-15 16:04:03 +0330

    date input

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Link } from "react-router-dom";
-import DatePicker from 'react-datepicker';
+// import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useState } from 'react';
 import camera from './images/camera.png';
@@ -10,8 +10,36 @@ import next from './images/next.png';
 function Profile(){
     const [Name,setName]=useState('Name')
     const [Username,setUsername]=useState('Username')
-    const [Date, setDate] = useState(null);
-    const [Camera,setCamera]=useState(camera)
+    const [Date, setDate] = useState('');
+    const [Gender, setGender] = useState('Female');
+    const [Camera,setCamera]=useState(camera);
+    const [confirm,setConfirm] = useState(false);
+
+    const [nameV,setNameV] = useState(true);
+    const [usernameV,setUsernameV] = useState(true);
+    const nameRegex = /^[a-zA-Z \-']+$/;
+    const usernameRegex = /^[a-zA-Z0-9_]{3,}$/;
+    function changeName(e){
+        const value = e.target.value;
+        const isValid = nameRegex.test(value);
+        if(value==="") {
+            setNameV(true)
+        }else{
+            setNameV(isValid);
+        }
+        setName(value);
+    }
+    function changeUsername(e){
+        const value = e.target.value;
+        const isValid = usernameRegex.test(value);
+        if(value==="") {
+            setUsernameV(true)
+        }else{
+            setUsernameV(isValid);
+        }
+        setUsername(value);
+    }
+
     function handleChange(e) {
         console.log(e.target.files);
         setCamera(URL.createObjectURL(e.target.files[0]));
@@ -33,26 +61,24 @@ function Profile(){
                     <div className='w-auto flex justify-between my-5 px-2 py-1'>
                         <div className='w-auto font-imprima text-gray-400'>Name</div>
                         <input className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}
-                            value={Name}/>
+                            value={Name} onChange={e => changeName(e)} onBlur={()=>{if(!nameV) alert('Valid Name:letters,spaces,and hyphens')}} placeholder='Name' type='text'/>
                     </div>
+                    {Name ==='' && confirm && (<div className='w-auto font-imprima text-right ml-auto text-red-500 text-xs'>You should fill the name.</div>)}
+                    {Name !=='' && !nameV && confirm && (<div className='w-auto font-imprima text-right ml-auto text-red-500 text-xs'>Invalid Name</div>)}
                     <div className='w-auto flex justify-between my-5 px-2 py-1'>
                         <div className='w-auto font-imprima text-gray-400'>Username</div>
                         <input className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}
-                            value={Username}/>
+                            value={Username} onChange={e => changeUsername(e)} onBlur={()=>{if(!usernameV) alert('Valid Username:letters,underscores,with a minimum length of 3 characters')}} type='text' placeholder='Username'/> 
                     </div>
+                    {Username ==='' && confirm && (<div className='w-auto font-imprima text-right text-red-500 text-xs'>You should fill the username.</div>)}
+                    {Username !=='' && !usernameV && confirm && (<div className='w-auto font-imprima text-right text-red-500 text-xs'>Invalid Username</div>)}
                     <div className='w-auto flex justify-between my-5 px-2 py-1'>
                         <div className='w-auto font-imprima text-gray-400 mr-3'>Date of birth</div>
-                        <DatePicker
-                            className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima'
-                            style={{ borderBottomWidth: '1.5px' }}
-                            selected={Date}
-                            onChange={date => setDate(date)}
-                            placeholderText='Select a Date'
-                        />
+                        <input type='date' value={Date} onChange={(event) => setDate(event.target.value)} className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima' style={{ borderBottomWidth: '1.5px' }}/>
                     </div>
                     <div className='w-auto flex justify-between my-5 px-2 py-1'>
                         <div className='w-auto font-imprima text-gray-400'>Gender</div>
-                        <select className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}>
+                        <select value={Gender} onChange={(event)=> setGender(event.target.value)} className='w-44 sm:w-64 outline-none border-b pl-3 border-gray-200 text-myDark font-imprima' style={{borderBottomWidth: '1.5px'}}>
                             <option value='female'>Female</option>
                             <option value='male'>Male</option>
                             <option value='others'>Others</option>
@@ -67,7 +93,7 @@ function Profile(){
                 </div>
                 <div className='w-auto flex justify-between mx-11'>
                     <div className='w-auto font-imprima text-gray-400 text-lg'>Skip</div>
-                    <div className='w-auto border-2 border-myOrange text-center font-imprima text-myOrange py-1 px-2 rounded-xl'>Done</div>
+                    <div className='w-auto border-2 border-myOrange text-center font-imprima text-myOrange py-1 px-2 rounded-xl' onClick={()=>setConfirm(true)}>Done</div>
                 </div>
             </div>
         </div>
