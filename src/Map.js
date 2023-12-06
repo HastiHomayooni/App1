da2aefef730567583d4adad8c30f37e4527d5c96
HastiHomayooni <homayooni.hasti@gmail.com>
2023-12-06 15:35:29 +0330

    get user location with move marker and search

--- src/Map.js
+++ src/Map.js
@@ -9,10 +9,16 @@ import axios from 'axios';
 
 import mark from '../src/images/mark.png'
 import back from '../src/images/back2.png'
+import search from '../src/images/search.png'
 
 import React from 'react';
 import './App.css';
 import { Link } from "react-router-dom";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/pagination';
+import { FreeMode, Scrollbar, Mousewheel} from 'swiper/modules';
+import 'swiper/css';
 
 const Mark = L.icon({
     iconUrl: mark,
@@ -22,7 +28,8 @@ const Mark = L.icon({
 function Map(){
     const [userLocation,setUserLocation] =useState([36.5626, 53.0602]);
     const [address,setAddress] =useState('')
-
+    const [addresses, setAddresses] = useState([]);
+    const [bool,setBool] = useState(false);
 
     const handleMarkerDrag =async (e) => {
         const LatLng=e.target.getLatLng();
@@ -42,7 +49,7 @@ function Map(){
         } catch (error) {
           console.error('Error fetching address:', error);
         }
-      };
+    };
     useEffect(() => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
@@ -60,16 +67,77 @@ function Map(){
         }
     }, []);
     
+    const handleChange= async (event) =>{
+        const inputText = event.target.value;
+        setAddress(inputText)
+        setBool(true)
+        if(inputText.trim()!==''){
+            try{
+                const response = await axios.get(
+                    `https://api.neshan.org/v1/search?term=${encodeURIComponent(inputText)}&lat=36.565546&lng=53.05`,
+                    {
+                      headers: {
+                        'Api-Key': 'service.JU5yHIPjoFIQD5DLUPmbEkVJf0rCyR8hLdQtWFNk'
+                      }
+                    }
+                );
+
+                const data = response.data;
+                const matchingAddresses = data.items;
+                setAddresses(matchingAddresses);
+
+            }catch(error){
+                console.error('Error fetching addresses:', error);
+            }
+        } else {
+            setAddresses([]);
+        }
+            addresses.map((address) =>(console.log(address)));
+    }
+
     return(
         <>
             <div className='w-screen h-screen flex flex-col items-center'>
-                <div className='absolute w-11/12 mx-auto flex justify-between bg-white m-4 p-3 z-10 rounded-2xl'>
+                <div className='absolute w-11/12 mx-auto flex justify-between bg-white m-4 p-1 z-10 rounded-2xl'>
                     <Link to={''}>
                         <img className='w-10 py-1' src={back} alt="back" />
                     </Link>
                 </div>
-                <div className='rounded-t-3xl p-4 h-auto w-11/12 mx-auto bg-white flex flex-col items-center justify-center absolute bottom-0 z-10'>
-                    <div>{address}</div>
+                <div className='rounded-t-3xl py-4 px-5 h-auto w-11/12 xl:w-9/12 mx-auto bg-white flex flex-col items-center justify-center absolute bottom-0 z-10'>
+                    <div className='w-3/4 flex flex-col items-center justify-center'>
+                        <div className='w-full flex p-1 pl-2 items-center justify-between bg-myOrange rounded-xl'>
+                            <img className='w-12' src={search} alt="search"/>
+                            <input className='w-4/5 px-4 py-3 rounded-xl outline-none text-xl text-center font-imprima text-black' onChange={handleChange} placeholder='Your Location' value={address}/>
+                        </div>
+                        {bool && address && addresses.length > 0 && (
+                            <div className='w-full px-4 overflow-hidden border-x-2 border-b-2 border-gray-200 rounded-b-xl h-auto max-h-20 mx-8 mb-7 flex flex-col justify-between items-end'>
+                                <Swiper
+                                    direction={'vertical'}
+                                    slidesPerView={'auto'}
+                                    freeMode={true}
+                                    scrollbar={true}
+                                    mousewheel={true}
+                                    modules={[FreeMode, Scrollbar, Mousewheel]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide>
+                                        {addresses.map((address) => (
+                                            <>
+                                                <div className='w-full text-lg my-2 font-imprima text-right cursor-pointer' 
+                                                    onClick={() =>{ setAddress(address.title);
+                                                        setBool(false);
+                                                        setUserLocation([address.location.y, address.location.x]);}} key={address.id}>{address.title}</div>
+                                            </>
+                                        ))}
+                                    </SwiperSlide>
+                                </Swiper>
+                            </div>
+                        )}
+                        <Link className='w-3/5 mt-10 mx-auto' to={''}>
+                            <div className='w-auto bg-gray-200 text-center font-imprima text-myDark p-2 rounded-xl hover:bg-myDark hover:text-gray-200'>Done</div>
+                        </Link>
+                    </div>
+
                 </div>
             </div>
             <MapContainer center={userLocation} zoom={14.5}
