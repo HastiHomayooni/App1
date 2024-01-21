1ae2848034efe56e842341039037d9ee0aa12113
HastiHomayooni <homayooni.hasti@gmail.com>
2024-01-21 17:32:05 +0330

    add new route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-
 import './App.css';
 import Start from '../src/Start';
 import SignUp from '../src/SignUp';
 import Login from '../src/Login';
 import Profile from '../src/Profile';
 import Specification from '../src/Specification';
+import FriendProfile from '../src/FriendProfile';
 import Map from '../src/Map';
 import Account from '../src/Account'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import {useState} from 'react';
+
 function App() {
+  const [OtherLocation,setOtherLocation] =useState([[36.58, 53.06],[36.556, 53.06],])
+  const [posts,setPosts] =useState([])
   return (
     <BrowserRouter>
       <Routes>
@@ -19,8 +23,9 @@ function App() {
           <Route path='Login' element={<Login/>}/>
           <Route path='Profile' element={<Profile/>}/>
           <Route path='Specification' element={<Specification/>}/>
-          <Route path='Map' element={<Map/>}/>
+          <Route path='Map' element={<Map OtherLocation={OtherLocation}/>}/>
           <Route path='Account' element={<Account/>}/>
+          <Route path='FriendProfile' element={<FriendProfile posts={posts}/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
