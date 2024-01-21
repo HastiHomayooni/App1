898c39d043d3d68192e85dc89cf1f70d8e32b61d
HastiHomayooni <homayooni.hasti@gmail.com>
2024-01-21 17:30:13 +0330

    test

--- src/__tests__/Map.test.js
+++ src/__tests__/Map.test.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-
-const axios = require('axios');
+import {BrowserRouter as Router} from 'react-router-dom';
+import {render, screen,fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom'
+import { act } from 'react-dom/test-utils';
 
 import Map from '../Map';
 
-test('Adding elements to DOM', async () => {
-  // Mock the axios get method to return a mock response
-  jest.spyOn(axios, 'get').mockResolvedValue({
-    data: {
-      items: [
-        { id: 1, title: 'Element 1' },
-        { id: 2, title: 'Element 2' },
-      ],
-    },
+const axios = require('axios');
+
+global.navigator.geolocation = {
+  getCurrentPosition: jest.fn(),
+};
+jest.mock('leaflet')
+jest.mock('react-leaflet', () => ({
+  MapContainer: () => <div>MapContainer</div>,
+  TileLayer: () => <div>TileLayer</div>,
+  Marker: () => <div>Marker</div>,
+}))
+
+jest.mock('axios', () => ({
+  get: jest.fn(() => Promise.resolve({data: {}})),
+}));
+jest.mock('swiper/react', () => ({
+  Swiper: ({children}) => <div>{children}</div>,
+  SwiperSlide: ({children}) => <div>{children}</div>,
+}));
+jest.mock('swiper', () => ({
+  FreeMode: (props) => null,
+  Mousewheel: (props) => null,
+  Scrollbar: (props) => null
+}))
+jest.mock('swiper/css/pagination', () => jest.fn());
+
+//The describe function is used to group the individual test cases for the Map component.
+describe('Map', () => {
+  test('add an element to DOM', () => {
+    render(<Router><Map /></Router>);
+    const addressInput = screen.getByPlaceholderText('Your Location');
+    expect(addressInput).toBeInTheDocument();
   });
-  // Render the component
-  render(<Map />);
-  // Wait for the async operation to finish
-  await waitFor(() => {
-    expect(screen.getByText('Element 1')).toBeInTheDocument();
-    expect(screen.getByText('Element 2')).toBeInTheDocument();
+  
+  test('updates an element when input changes', () => {
+    render(<Router><Map /></Router>);
+    const addressInput = screen.getByPlaceholderText('Your Location');
+    //The act() is for wrapping interactions with components and handling any resulting side effects
+    //to ensure that React has a chance to update the component
+    act(() => {
+      fireEvent.change(addressInput, { target: { value: 'New York' } });
+    });
+    expect(addressInput.value).toBe('New York');
+  });
+
+  test('deletes an element from DOM', () => {
+    render(<Router><Map /></Router>);
+    const addressInput = screen.getByPlaceholderText('Your Location');
+    act(() => {
+      fireEvent.change(addressInput, { target: { value: '' } });
+    });
+    expect(addressInput.value).toBe('');
   });
 });
+
