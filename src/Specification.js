3b6cea15fb25616a0602476be23fc760f03415cc
HastiHomayooni <homayooni.hasti@gmail.com>
2024-01-23 02:45:45 +0330

    show specification that was saved in database

--- src/Specification.js
+++ src/Specification.js
@@ -4,12 +4,66 @@ import Hobby from '../src/images/Hobby.jpg'
 // import back from '../src/images/back.png'
 import { Link } from "react-router-dom";
 import { useState } from 'react';
+import axios from 'axios';
 
-function Specification (){
-    const [favorites,setFavorites] =useState('')
-    const [hobbies,setHobbies] =useState('')
-    const [education,setEducation] =useState('')
-    const [job,setJob] =useState('')
+function Specification ({user,setUser}){
+    const [favorites,setFavorites] =useState(user.user.favorites)
+    const [hobbies,setHobbies] =useState(user.user.hobbies)
+    const [education,setEducation] =useState(user.user.education)
+    const [job,setJob] =useState(user.user.job)
+
+    // if(user.user.favorites !=null){
+    //     setFavorites(user.user.favorites)
+    // }
+    // if(user.user.hobbies !=null){
+    //     setHobbies(user.user.hobbies)
+    // }
+    // if(user.user.education !=null){
+    //     setEducation(user.user.education)
+    // }
+    // if(user.user.job !=null){
+    //     setJob(user.user.job)
+    // }
+
+    const name=user.user.name
+    const username=user.user.username
+    const email=user.user.email
+    const password=user.user.password
+    const user_id=user.user.user_id
+    // console.log(user)
+
+    const specification = async () => {
+        console.log("speee")
+        try {
+            console.log("oomad")
+            axios.defaults.headers.put['Content-Type'] = 'application/json';
+            const response = await axios.put(
+                'http://localhost:5000/UserManagement',
+                {
+                    User: {
+                        user_id: user_id,
+                        username: username,
+                        password: password,
+                        email: email,
+                        name: name,
+                        favorites: favorites,
+                        hobbies: hobbies,
+                        education: education,
+                        job: job
+                    }
+                }
+            );
+            const status = response.status;
+            const data = response.data
+            console.log(data)
+            console.log(status)
+            if(status === 200){
+                setUser(data)
+            }
+        } catch (error) {
+            console.error('Error fetching address:', error);
+        }
+      };
     return (
         <div className='w-screen h-screen bg-myDark flex flex-col items-center'>
             <div className='w-full flex mt-5 px-8'>
@@ -38,7 +92,7 @@ function Specification (){
                 {(job==='' || education==='' || hobbies==='' || favorites==='') && (<div className='w-auto text-myOrange font-imprima text-left text-xs pr-10'>Please fill these boxes to help find friends with similar personality</div>)}
 
                 <Link className='w-auto mt-5 ml-auto xl:px-48' to={'/Profile'}>
-                    <div className='w-auto border-2 border-myOrange text-center font-imprima text-myOrange py-1 px-2 rounded-xl hover:bg-myOrange hover:text-myDark'>Done</div>
+                    <div className='w-auto border-2 border-myOrange text-center font-imprima text-myOrange py-1 px-2 rounded-xl hover:bg-myOrange hover:text-myDark' onClick={specification}>Done</div>
                 </Link>
             </div>
             <div className='w-full overflow-hidden'>
