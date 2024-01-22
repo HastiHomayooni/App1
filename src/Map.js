dca7f0138adafd5dd038d1ae3614b1bb3fbe144e
HastiHomayooni <homayooni.hasti@gmail.com>
2024-01-23 02:49:43 +0330

    show friends that are located near us

--- src/Map.js
+++ src/Map.js
@@ -30,11 +30,76 @@ const Mark = L.icon({
   iconAnchor: [16, 32],
 });
 
-function Map({OtherLocation}) {
+function Map({user,setUser,OtherLocation,setOtherLocation,setUserArray}) {
   const [userLocation, setUserLocation] = useState([36.5626, 53.0602]);
   const [address, setAddress] = useState('')
   const [addresses, setAddresses] = useState([]);
   const [bool, setBool] = useState(false);
+  let id=0
+
+  const name=user.user.name
+  const username=user.user.username
+  const email=user.user.email
+  const password=user.user.password
+  const user_id=user.user.user_id
+  const favorites=user.user.favorites
+  const hobbies=user.user.hobbies
+  const education=user.user.education
+  const job=user.user.job
+  const date=user.user.date
+  const gender=user.user.gender
+  
+  const done =async()=>{
+    console.log("speee")
+    if(user.locations !=null){
+    if(user.locations.loc_description !==''){
+      console.log("heeee")
+      setAddress(user.locations.loc_description)
+      id=user.locations.location_id
+      }
+    }  
+        try {
+            console.log("oomad")
+            console.log(userLocation["lat"])
+            axios.defaults.headers.put['Content-Type'] = 'application/json';
+            const response = await axios.put(
+                'http://localhost:5000/UserManagement',
+                {
+                    User: {
+                        user_id: user_id,
+                        username: username,
+                        password: password,
+                        email: email,
+                        name: name,
+                        favorites: favorites,
+                        hobbies: hobbies,
+                        education: education,
+                        job: job,
+                        gender:gender,
+                        date:date
+                    },
+                    Locations:{
+                      location_id:id,
+                      x:userLocation.lat,
+                      y:userLocation.lng,
+                      loc_description:address
+                    }
+                }
+            );
+            const status = response.status;
+            const data = response.data
+            console.log(data)
+            console.log(status)
+            if(status === 200){
+                setUser(data)
+                setOtherLocation(data.otherLocation)
+                console.log(OtherLocation)
+            }
+        } catch (error) {
+            console.error('Error fetching address:', error);
+        }
+        console.log(OtherLocation)
+  }
 
   const handleMarkerDrag = async (e) => {
     const LatLng = e.target.getLatLng();
@@ -101,7 +166,7 @@ function Map({OtherLocation}) {
   }
 
   const navigate = useNavigate();
-  console.log(navigate)
+
   return (
     <>
       <div className='w-screen h-screen flex flex-col items-center'>
@@ -148,19 +213,19 @@ function Map({OtherLocation}) {
             </div>
           </div>
         </div>
-        <Link className='w-5/12 sm:w-3/12 mx-auto z-10 mt-auto mb-16' to={'/Account'}>
+        <Link className='w-5/12 sm:w-3/12 mx-auto z-10 mt-auto mb-16' to={''}>
           <div
-            className='rounded-3xl py-4 w-full bg-myDark hover:brightness-150 text-center text-lg text-white font-imprima'>Done
+            className='rounded-3xl py-4 w-full bg-myDark hover:brightness-150 text-center text-lg text-white font-imprima' onClick={done}>Find Friends
           </div>
         </Link>
         <div className='fixed py-3 z-10 px-7 sm:mx-auto flex justify-between items-center bottom-0 w-full bg-slate-100'>
-          <Link className='w-auto' to={''}>
+          <Link className='w-auto' to={'/Map'}>
               <img src={botLoc} alt='location' className='w-4/5'/>
           </Link>
           <Link className='w-auto' to={''}>
               <img src={botChat} alt='chat' className='w-4/5'/>
           </Link>
-          <Link className='w-auto' to={''}>
+          <Link className='w-auto' to={'/Account'}>
               <img src={botUser} alt='profile' className='w-4/5'/>
           </Link>
         </div>
@@ -174,7 +239,7 @@ function Map({OtherLocation}) {
         {OtherLocation.map((location, index) => (
           <Link to={'/FriendProfile'} key={index}>
             <Marker
-              position={location}
+              position={[location.locations.x,location.locations.y]}
               icon={L.icon({
                 iconUrl: friendLoc,
                 iconSize: [40, 40],
@@ -182,6 +247,7 @@ function Map({OtherLocation}) {
               })}
               eventHandlers={{
                 click: () => {
+                  setUserArray(location)
                   navigate('/FriendProfile');
                 },
               }}
